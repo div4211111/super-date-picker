96fe8848da6ac34bbb6d2a98a377c0ff26eb295a
div4211111 <andrew@Andrews-MacBook-Pro.local>
2023-06-06 15:16:37 +0300

    fix: tiny fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,90 @@
 import "react-datepicker/dist/react-datepicker.css";
-import { DatePicker, RelativeTimePicker, SetNow } from "./components";
+import { SuperDatePicker } from "./components";
 import { useState } from "react";
+import {
+  DurationRange,
+  OnRefreshChangeProps,
+  OnRefreshProps,
+  OnTimeChangeProps,
+  ShortDate,
+} from "@shared/types";
+import { DateInput } from "@components/DateInput/DateInput";
 function App() {
-  const [date, setDate] = useState(new Date());
-  const [isNow, setIsNow] = useState(false);
+  const [recentlyUsedRanges, setRecentlyUsedRanges] = useState<DurationRange[]>(
+    []
+  );
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [start, setStart] = useState<ShortDate>("now-30m");
+  const [end, setEnd] = useState<ShortDate>("now");
+  const [isPaused, setIsPaused] = useState<boolean>(true);
+  const [refreshInterval, setRefreshInterval] = useState<number>(0);
+
+  const onTimeChange = ({ start, end }: OnTimeChangeProps) => {
+    const recentlyUsedRange = recentlyUsedRanges.filter((recentlyUsedRange) => {
+      const isDuplicate =
+        recentlyUsedRange.start === start && recentlyUsedRange.end === end;
+      return !isDuplicate;
+    });
+    recentlyUsedRange.unshift({ start, end });
+    setStart(start);
+    setEnd(end);
+    setRecentlyUsedRanges(
+      recentlyUsedRange.length > 10
+        ? recentlyUsedRange.slice(0, 9)
+        : recentlyUsedRange
+    );
+    setIsLoading(true);
+    startLoading();
+  };
+
+  const onRefresh = async ({ start, end, refreshInterval }: OnRefreshProps) => {
+    return new Promise((resolve) => {
+      setTimeout(resolve, 100);
+    }).then(() => {
+      console.log(start, end, refreshInterval);
+    });
+  };
+
+  const startLoading = () => {
+    setTimeout(stopLoading, 1000);
+  };
+
+  const stopLoading = () => {
+    setIsLoading(false);
+  };
+
+  const onRefreshChange = ({
+    isPaused,
+    refreshInterval,
+  }: OnRefreshChangeProps) => {
+    setIsPaused(isPaused);
+    setRefreshInterval(refreshInterval);
+  };
   return (
-    <div style={{ display: "flex", flexDirection: "column" }}>
-      <div style={{ alignSelf: "flex-start" }}>
-        <DatePicker date={date} setDate={setDate} name={"Start date"} />
-        <RelativeTimePicker date={date} setDate={setDate} name={"Start date"} />
-        <SetNow setIsNow={setIsNow} type={"start"} />
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: "20px",
+        padding: "20px",
+      }}
+    >
+      <div>
+        <SuperDatePicker
+          start={start}
+          end={end}
+          onTimeChange={onTimeChange}
+          onRefreshChange={onRefreshChange}
+          onRefresh={onRefresh}
+          isLoading={isLoading}
+          isPaused={isPaused}
+          refreshInterval={refreshInterval}
+          recentlyUsedRanges={recentlyUsedRanges}
+        />
+      </div>
+      <div style={{ display: "flex", gap: "8px" }}>
+        <DateInput name={"start"} date={start} />
+        <DateInput name={"end"} date={end} />
       </div>
     </div>
   );
